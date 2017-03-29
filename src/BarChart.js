c7ee1870c4229a8965e9778f93d2428b2fbd81cb
Eric Chen <ericchen0121@Erics-MacBook-Pro.local>
2017-03-29 00:14:39 -0700

    react d3 with victory charts. barchart component created

---
+++ src/BarChart.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { VictoryBar, VictoryChart, VictoryAxis } from 'victory';
+
+class BarChart extends Component {
+  render() {
+    const {data, xData, yData} = this.props;
+
+    return (
+      <VictoryChart
+        domainPadding={20}
+      >
+        <VictoryAxis
+          tickFormat={data.map((d) => d[xData])}
+        />
+        <VictoryAxis
+          dependentAxis
+          tickFormat={(x) => `$${x/1000}k`}
+        />
+        <VictoryBar
+          data={data}
+          x={xData}
+          y={yData}
+        />
+      </VictoryChart>
+    )
+  }
+}
+
+export default BarChart;
---
+++ src/LineChart.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { VictoryLine, VictoryChart, VictoryAxis } from 'victory';
+
+class LineChart extends Component {
+  
+}
+
+export default LineChart;
